a4414b437dc79e75e44409407c822d7fff09c2cc
othAs-dev <othmane.stig@gmail.com>
2024-08-02 11:31:57 +0200

    now we can find a user and update its infos

--- front/src/app/config/guards/auth.guard.ts
+++ front/src/app/config/guards/auth.guard.ts
@@ -1,6 +1,6 @@
-import { CanActivateFn, Router } from '@angular/router';
-import { inject } from '@angular/core';
-import {switchMap, of, map} from 'rxjs';
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {map} from 'rxjs';
 import {AuthService} from "@app/auth/auth.service";
 
 export const authGuard: CanActivateFn = () => {
